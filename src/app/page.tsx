2c66aae4ef7d26a51de41c2e7652dc44a20406bf
Izza Wildan Ridhoni <izza.wildan@ptsji.id>
2025-06-25 15:20:33 +0700

    add timeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,15 @@ import Features01Page from "@/components/features-01/features-01";
 import Hero01 from "@/components/hero-01/hero-01";
 import { Hero1 } from "@/components/hero1";
 import { Navbar1 } from "@/components/navbar1";
+import Timeline from "@/components/timeline-04/timeline-04";
 
 export default function Home() {
   return (
     <>
       <Navbar1 />
-      {/* <Hero1
-        heading="Your Friendly Fullstack Web Developer "
-        description="I design and build full-featured web apps using React, Tailwind, and Laravel. Clean code, modern UI, and great performance — all in one package."
-        image={{ src: "coffee.png", alt: "" }}
-      /> */}
       <Hero01 />
       <Features01Page />
+      <Timeline />
     </>
   );
 }
