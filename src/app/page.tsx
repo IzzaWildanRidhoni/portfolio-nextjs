269fb651ad7165d10090964fb9e11c2df0dcf837
Izza Wildan Ridhoni <izza.wildan@ptsji.id>
2025-06-25 15:23:44 +0700

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Features01Page from "@/components/features-01/features-01";
 import Hero01 from "@/components/hero-01/hero-01";
-import { Hero1 } from "@/components/hero1";
 import { Navbar1 } from "@/components/navbar1";
 import Timeline from "@/components/timeline-04/timeline-04";
+import Footer from "@/components/footer-04/footer-04";
 
 export default function Home() {
   return (
@@ -11,6 +11,7 @@ export default function Home() {
       <Hero01 />
       <Features01Page />
       <Timeline />
+      <Footer />
     </>
   );
 }
