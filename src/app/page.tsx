e0898e52fa92a483d92284bda9c538d4ad61b8d5
Izza Wildan Ridhoni <izza.wildan@ptsji.id>
2025-06-25 15:45:42 +0700

    add portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
-import Features01Page from "@/components/features-01/features-01";
-import Hero01 from "@/components/hero-01/hero-01";
+import Service from "@/components/features-01/features-01";
+import Hero from "@/components/hero-01/hero-01";
 import Timeline from "@/components/timeline-04/timeline-04";
 import Footer from "@/components/footer-04/footer-04";
 import Testimonial from "@/components/testimonial-06/testimonial-06";
 import Contact from "@/components/contact-03/contact-03";
-import Navbar2 from "@/components/navbar-02/navbar-02";
+import Navbar from "@/components/navbar-02/navbar-02";
+import Portfolio from "@/components/features-02/features-02";
 
 export default function Home() {
   return (
     <>
-      <Navbar2 />
-      <Hero01 />
-      <Features01Page />
+      <Navbar />
+      <Hero />
+      <Service />
+      <Portfolio />
       {/* <Timeline /> */}
       <Testimonial />
       <Contact />
