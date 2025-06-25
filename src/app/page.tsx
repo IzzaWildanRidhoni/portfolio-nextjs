ca2ad701b8e36497d2d5ff8add936e6ea510b6ca
Izza Wildan Ridhoni <izza.wildan@ptsji.id>
2025-06-25 15:38:36 +0700

    update navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Features01Page from "@/components/features-01/features-01";
 import Hero01 from "@/components/hero-01/hero-01";
-import { Navbar1 } from "@/components/navbar1";
 import Timeline from "@/components/timeline-04/timeline-04";
 import Footer from "@/components/footer-04/footer-04";
 import Testimonial from "@/components/testimonial-06/testimonial-06";
@@ -10,7 +9,6 @@ import Navbar2 from "@/components/navbar-02/navbar-02";
 export default function Home() {
   return (
     <>
-      {/* <Navbar1 /> */}
       <Navbar2 />
       <Hero01 />
       <Features01Page />
