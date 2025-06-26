f496acc862b064832438c7e4568498d7074ef8fb
Izza Wildan Ridhoni <izza.wildan@ptsji.id>
2025-06-26 07:56:24 +0700

    add blogs

---
+++ src/app/(user)/blogs/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navbar-02/navbar-02";
+import Footer from "@/components/footer-04/footer-04";
+import { Blog8 } from "@/components/blog8";
+
+export default function BlogPage() {
+  return (
+    <>
+      <Navbar />
+      <Blog8 />
+      <Footer />
+    </>
+  );
+}
