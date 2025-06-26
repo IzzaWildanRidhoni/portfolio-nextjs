5299e6ec27be7eeada871fa90d76932ef54b1bdf
Izza Wildan Ridhoni <izza.wildan@ptsji.id>
2025-06-26 07:45:48 +0700

    add dashboard and admin

---
+++ src/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,114 @@
+export default function DashboardPage() {
+  return (
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero odit
+      beatae maiores illum error delectus excepturi, a provident? Similique quo
+      id perspiciatis, excepturi commodi tenetur est ab placeat laborum neque,
+      sed maxime quaerat ipsum. Voluptatibus minus laborum porro quos voluptate
+      delectus tenetur provident, voluptatum amet dolorum, recusandae nostrum
+      alias possimus labore magni odio est dolorem, autem atque? Repudiandae
+      odit, beatae vitae est, nesciunt, a placeat quo officiis sunt cum dolore?
+      Voluptatum quibusdam quisquam adipisci eum sit accusamus amet explicabo
+      repellat pariatur? Impedit necessitatibus dolores soluta, nemo incidunt
+      sit molestias quia voluptatibus eos, ipsa quas corrupti nostrum numquam.
+      Necessitatibus fugit possimus maxime ex quisquam accusamus corrupti
+      perspiciatis rem quam? Quasi cumque molestiae iste odit aliquam voluptatem
+      at possimus eos quibusdam dolore. Fuga sed vitae eveniet quos laborum
+      iusto ab neque autem explicabo. Eaque tempora animi quaerat molestiae amet
+      tempore, id aspernatur ex eos quasi temporibus odit nihil. Repudiandae in
+      suscipit corporis odio, aperiam id debitis quisquam. Nulla minima
+      doloribus sint assumenda harum repudiandae et nam exercitationem,
+      accusamus error aperiam sunt saepe explicabo quidem molestiae quos
+      distinctio autem voluptatibus cupiditate unde est alias deserunt! Aperiam
+      est nemo quod ipsam aut, repellat ex, iure animi quae incidunt fugiat
+      architecto labore accusamus natus tempore corrupti excepturi neque saepe
+      fugit rem repellendus consequatur unde fuga possimus. Facilis repellat
+      praesentium suscipit! Fugit atque voluptatibus quidem qui, porro incidunt
+      velit aut corrupti voluptate tenetur ea earum blanditiis exercitationem
+      quos harum ipsa, a officia repudiandae excepturi! Eius odit recusandae
+      qui, eaque, magni minima hic libero facere accusantium ipsum repudiandae
+      quidem commodi aliquam eligendi odio, delectus nulla vitae! Nobis ad esse
+      ex at sit fuga distinctio quos sapiente aliquam dolores saepe sunt odio,
+      asperiores tempora quae. Nesciunt qui sunt veritatis aperiam itaque
+      repudiandae minima quia vel tempora repellendus? Temporibus eveniet
+      aperiam iure laboriosam explicabo ea officia! Voluptatibus hic ex culpa
+      harum totam blanditiis provident repellat debitis quos sequi, impedit
+      recusandae fugit numquam dolorem aliquam perferendis pariatur eius soluta
+      magni quam labore beatae architecto minima! Deserunt autem qui assumenda
+      tempora est quia earum, consequuntur provident ipsum aspernatur corporis
+      nulla minus! Odit reprehenderit consequuntur architecto quia accusantium.
+      Temporibus, molestias repellat velit harum adipisci, facilis cum, fuga
+      libero corporis dolores porro magni doloribus nihil quasi quis rerum
+      voluptate iste voluptatem commodi. Molestiae itaque reiciendis facilis
+      culpa, dolorem ea, pariatur officiis provident beatae neque fugiat animi
+      quas, officia ad vitae quidem temporibus fuga! Dignissimos enim dicta,
+      doloribus eveniet quaerat facere nemo reiciendis dolorum neque?
+      Accusantium enim nulla eum voluptate neque? Libero ea aliquid eligendi,
+      voluptates veritatis doloremque debitis excepturi obcaecati laborum neque
+      expedita aliquam quibusdam soluta vel. Nisi accusamus aperiam ipsa
+      aliquam. Eveniet quisquam beatae corrupti molestiae molestias. Amet
+      tempora porro obcaecati nesciunt qui magni deleniti maiores, omnis
+      temporibus, est sequi, vero nam? Quos, itaque voluptates ratione, tenetur
+      possimus sit facilis veniam pariatur eaque suscipit nam delectus
+      necessitatibus aspernatur ut optio facere quidem unde. Distinctio quidem
+      delectus earum voluptate sapiente, sunt provident atque repellendus a
+      natus, illo reprehenderit officia est ab accusantium maiores
+      exercitationem veritatis dolorem? Dicta numquam explicabo commodi minima
+      doloribus magnam veniam consequuntur, accusantium dolorem, quidem nostrum
+      id eos iste similique at totam eligendi est nihil! Ab, accusamus
+      perspiciatis deserunt qui temporibus, iure nobis quisquam omnis veniam
+      molestias, aliquam similique deleniti quod dignissimos itaque officia
+      possimus laborum culpa nemo labore earum esse mollitia. Minus quis
+      accusantium incidunt commodi deleniti eligendi necessitatibus natus,
+      blanditiis quibusdam molestias repellat. Quo architecto obcaecati eum
+      commodi dolores atque quas. Veritatis quas modi, dicta, architecto ipsam
+      repellat vel, laboriosam non molestias quisquam excepturi quos cum illo
+      autem voluptatum. Rerum atque iste molestiae porro ullam nulla qui nobis
+      possimus sit dignissimos illo sunt, in ab rem voluptatibus excepturi
+      repudiandae voluptatem natus deserunt quae? Dolore inventore maxime
+      consequuntur adipisci voluptas itaque quis necessitatibus facilis,
+      incidunt cupiditate fuga ducimus iste iure voluptatibus id minima
+      exercitationem amet, ipsum nulla dolorem sunt? Quaerat, laudantium. Id cum
+      aspernatur corporis modi neque ut deleniti molestias quae rerum
+      cupiditate, eligendi incidunt deserunt ipsum facilis! Officiis, cupiditate
+      dignissimos corporis fuga nemo fugit facere, necessitatibus quam labore
+      dolorum non earum rem similique. At a, harum sed iste illo ad quas ratione
+      nostrum modi labore voluptates odio consequuntur ipsam inventore
+      voluptatibus? Aut pariatur in officia tenetur! Quod omnis, optio earum
+      accusantium ad unde est ipsam eveniet, eos neque quos numquam reiciendis
+      explicabo quam, quaerat totam ratione velit repellendus nam illo
+      voluptatum soluta! Id suscipit iste beatae dolore esse hic tempore nostrum
+      aperiam! Ullam incidunt ducimus error molestiae consequuntur?
+      Reprehenderit nobis est exercitationem molestias repellat? Tempora vero
+      tenetur modi illum quos doloribus accusantium, dignissimos deleniti beatae
+      rerum odio debitis, deserunt quia veniam ab illo? Dolor, placeat? Dolores
+      quos reiciendis magnam qui alias explicabo facere, non corrupti excepturi
+      eaque beatae reprehenderit sapiente praesentium aspernatur cum distinctio
+      expedita vel assumenda accusamus odit consectetur itaque repudiandae
+      deleniti nobis. Quam reprehenderit quod assumenda deleniti aliquid!
+      Accusantium, deserunt? Molestiae quaerat deleniti praesentium nostrum
+      reprehenderit sequi odit numquam doloremque repellendus, exercitationem
+      eligendi soluta sunt itaque impedit id quod iste aliquid vero voluptate a
+      omnis possimus quibusdam! Fugiat iste veniam blanditiis, hic illum impedit
+      quod quasi. Dolor nam ipsa atque minus exercitationem qui eaque esse?
+      Dolorum enim, aliquam vitae error quaerat deleniti fugiat omnis quia,
+      optio commodi voluptate, accusamus voluptatum inventore et ipsa dicta.
+      Maiores dolor libero praesentium mollitia assumenda tempora, provident
+      voluptates nulla itaque et quae eligendi tempore ut? Optio, perspiciatis.
+      Odio illum quis deleniti, consequuntur reiciendis ipsum officiis aperiam
+      cupiditate quidem omnis nesciunt vel tempore minus, molestias placeat
+      error blanditiis inventore, dolores accusamus nostrum? Nihil quas quos
+      ullam eos debitis odit corrupti aspernatur laborum! Iusto, ratione laborum
+      magnam voluptatum architecto quia blanditiis deleniti reprehenderit
+      molestiae nisi quae fuga perspiciatis eos neque incidunt harum rerum saepe
+      exercitationem tempora officia deserunt quibusdam ipsam! Architecto
+      doloremque ipsam sunt vero cupiditate tenetur quia consequatur ducimus,
+      aliquid, pariatur blanditiis tempora illo sed. Dolore impedit totam facere
+      ipsa. Nobis esse maxime amet sed molestias officia consequatur corrupti
+      exercitationem, magnam necessitatibus earum eveniet ab. Quis iusto placeat
+      minus dolores, mollitia tenetur natus omnis, fuga sed ex reiciendis
+      exercitationem et quibusdam dolor quidem. Quasi facere necessitatibus
+      libero explicabo labore amet. Sint, suscipit.
+    </p>
+  );
+}
---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,53 @@
+// app/(admin)/layout.tsx
+
+import { ReactNode } from "react";
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+export default function AdminLayout({ children }: { children: ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+
+      <SidebarInset>
+        {/* Top Header */}
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">Admin</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Dashboard</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+
+        {/* Main Page Content */}
+        <main className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</main>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
